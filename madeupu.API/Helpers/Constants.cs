0347016543bb9d9ceb2381b042340f9c822d3ce8
JustoJaramillo <justoj.jaramillo@gmail.com>
2021-10-10 15:53:56 -0500

    Adding blob

--- madeupu.API/Helpers/Constants.cs
+++ madeupu.API/Helpers/Constants.cs
@@ -8,6 +8,6 @@ namespace madeupu.API.Helpers
     public static class Constants
     {
         public static readonly string NoImage = "https://localhost:44351/images/noimage.png";
-        public static readonly string ImageUrl = "https://vehicleapistorage.blob.core.windows.net";
+        public static readonly string ImageUrl = "https://madeupu.blob.core.windows.net";
     }
 }

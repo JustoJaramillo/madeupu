1d4cb7d785cb83b0a6e2c52205d76eccfe255c76
JustoJaramillo <justoj.jaramillo@gmail.com>
2021-10-11 00:07:32 -0500

    no image added

--- madeupu.API/Helpers/Constants.cs
+++ madeupu.API/Helpers/Constants.cs
@@ -7,7 +7,7 @@ namespace madeupu.API.Helpers
 {
     public static class Constants
     {
-        public static readonly string NoImage = "https://localhost:44351/images/noimage.png";
+        public static readonly string NoImage = "https://localhost:44318/images/noimage.png";
         public static readonly string UserImage = "https://madeupu.blob.core.windows.net/users/";
         public static readonly string ProjectImage = "https://madeupu.blob.core.windows.net/projects/";
     }

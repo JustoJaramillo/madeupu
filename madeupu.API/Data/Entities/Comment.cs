f9b9fbb586d7d5195403ed15b078021a89f14d5a
KEVIN ANDRES ORREGO CASTAÑEDA <kevinorrego251539@correo.itm.edu.co>
2021-10-18 21:22:10 -0500

    updating comments table inside database

--- madeupu.API/Data/Entities/Comment.cs
+++ madeupu.API/Data/Entities/Comment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace madeupu.API.Data.Entities
@@ -9,12 +10,28 @@ namespace madeupu.API.Data.Entities
     public class Comment
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int ProjectId { get; set; }
 
         [Display(Name = "Comentario")]
         [MaxLength(200, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Message { get; set; }
+
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime DateLocal => Date.ToLocalTime();
+
+        [JsonIgnore]
+        [Display(Name = "Usuario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public User User { get; set; }
+
+        [Display(Name = "Proyecto")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Project Project { get; set; }
+
     }
 }
